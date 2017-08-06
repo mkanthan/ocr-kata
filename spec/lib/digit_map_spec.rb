8e33c00afaea42620e00c93ce03a55c232f47e1d
Manu Kanthan <m.kanthan@gmail.com>
2017-08-06 13:55:19 -0400

    Map the digits to constant values

---
+++ spec/lib/digit_map_spec.rb
@@ -0,0 +1,21 @@
+require 'spec_helper'
+require_relative '../support/digit_helper'
+
+RSpec.describe DigitMap do
+  before do
+    @digits = DigitHelper::setup_digits
+  end
+
+  it "should set up the correct constant values for the digit slices" do
+    expect(DigitMap::ZERO).to eq(@digits["0"])
+    expect(DigitMap::ONE).to eq(@digits["1"])
+    expect(DigitMap::TWO).to eq(@digits["2"])
+    expect(DigitMap::THREE).to eq(@digits["3"])
+    expect(DigitMap::FOUR).to eq(@digits["4"])
+    expect(DigitMap::FIVE).to eq(@digits["5"])
+    expect(DigitMap::SIX).to eq(@digits["6"])
+    expect(DigitMap::SEVEN).to eq(@digits["7"])
+    expect(DigitMap::EIGHT).to eq(@digits["8"])
+    expect(DigitMap::NINE).to eq(@digits["9"])
+  end
+end
